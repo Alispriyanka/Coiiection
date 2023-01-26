a63528d6c06622eac80d014ff775540bef8307fa
ALIS PRIYANKA <alispriyanka@gmail.com>
2023-01-26 23:46:28 +0530

    updated

---
+++ Mhash1.java
@@ -0,0 +1,11 @@
+import java.util.*;  
+public class Mhash1{  
+  public static void main(String[]args){
+    HashSet<String> as=new HashSet<String>();
+    as.add("alis");
+    as.add("ammu");
+    as.add("antony");
+    as.add("asha");
+    System.out.println(as);
+}
+}
