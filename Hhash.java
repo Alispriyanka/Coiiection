f119bde4168e1bc9700caa751b9c61c5dc8575ed
ALIS PRIYANKA <alispriyanka@gmail.com>
2023-01-28 22:24:55 +0530

    test1

---
+++ Hhash.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Hhash {
+public static void main(String[] args) {
+    HashSet<String> hs= new HashSet<String>();
+      hs.add("suji");
+      hs.add("suga");
+      hs.add("juli");
+      hs.add("pappu");
+      hs.add("viji");
+System.out.println(hs);
+}
+}     
