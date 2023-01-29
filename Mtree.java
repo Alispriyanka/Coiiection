04ade8489b806165410419eaba10e5b8b6a4bde6
ALIS PRIYANKA <alispriyanka@gmail.com>
2023-01-29 16:53:37 +0530

    upload

---
+++ Mtree.java
@@ -0,0 +1,11 @@
+import java.util.*;  
+public class Mtree{  
+  public static void main(String[]args){
+    TreeSet<String> as=new TreeSet<String>();
+    as.add("alis");
+    as.add("ammu");
+    as.add("antony");
+    as.add("asha");
+    System.out.println(as);
+}
+}
