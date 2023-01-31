b78b0035cb3ee0ca7a1ab169c8eaa68849a6d691
ALIS PRIYANKA <alispriyanka@gmail.com>
2023-01-31 12:48:48 +0530

    test

---
+++ Desoraa.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Desoraa{
+public static void main(String[]aaa){
+ArrayList <String> List=new ArrayList  <String>();
+List.add("mani");
+List.add("asha");
+List.add("siva");
+List.add("ravi");
+List.add("jega");
+List.add("julie");
+List.add("ali");
+System.out.println("unsorted name:"+List);
+Collections.sort(List,Collections.reverseOrder());
+System.out.println("desending name:"+List);
+}}
+
